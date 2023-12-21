d3e9425dc47709b8ddc3e9331fbf553cd8c0069f
Philip Phlek <pphlek@gmail.com>
2023-12-21 01:38:29 -0500

    solved leetcode middle of linked list, fix comments

--- arrays/easy/leetcode/build-array-from-permutation.ts
+++ arrays/easy/leetcode/build-array-from-permutation.ts
@@ -7,7 +7,6 @@ build an array ans of the same length where ans[i] = nums[nums[i]] for each 0 <=
 A zero-based permutation nums is an array of distinct integers from 0 to nums.length - 1 (inclusive).
 
 Example 1:
-
 Input: nums = [0,2,1,5,3,4]
 Output: [0,1,2,4,5,3]
 Explanation: The array ans is built as follows: 
