1056ce92d749c0a424b53b4115b0f3b06d0bf337
Philip Phlek <pphlek@gmail.com>
2023-12-23 10:05:55 -0500

    solved leetcode reverse linked list

---
+++ linked-lists/easy/leetcode/reverse-linked-list.ts
@@ -0,0 +1,57 @@
+/*
+https://leetcode.com/problems/reverse-linked-list/
+
+Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+Example 1:
+Input: head = [1,2,3,4,5]
+Output: [5,4,3,2,1]
+
+Example 2:
+Input: head = [1,2]
+Output: [2,1]
+
+Example 3:
+Input: head = []
+Output: []
+ 
+Constraints:
+The number of nodes in the list is the range [0, 5000].
+-5000 <= Node.val <= 5000
+*/
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function reverseList(head: ListNode | null): ListNode | null {
+  let prev: ListNode | null = null
+  let curr: ListNode | null = head
+  while (curr) {
+    const next: ListNode | null = curr.next
+    curr.next = prev
+    prev = curr
+    curr = next
+  }
+  return prev
+}
+
+function reverseListRecursive(
+  head: ListNode | null,
+  prev: ListNode | null
+): ListNode | null {
+  if (!head) {
+    return prev
+  }
+  const next = head.next
+  head.next = prev
+  return reverseListRecursive(next, head)
+}
