78684e798cb01eb0c73a7c78a9739e800ca39888
Philip Phlek <pphlek@gmail.com>
2024-01-05 18:45:51 -0500

    solved algoexpert move element to end

---
+++ arrays/medium/algoexpert/move-element-to-end.ts
@@ -0,0 +1,34 @@
+/*
+https://www.algoexpert.io/questions/move-element-to-end
+
+You're given an array of integers and an integer. Write a function that moves 
+all instances of that integer in the array to the end of the array and returns 
+the array.
+
+The function should perform this in place (i.e., it should mutate the input
+array) and doesn't need to maintain the order of the other integers.
+
+Sample Input
+array = [2, 1, 2, 2, 2, 3, 4, 2]
+toMove = 2
+
+Sample Output
+[1, 3, 4, 2, 2, 2, 2, 2] // the numbers 1, 3, and 4 could be ordered differently
+*/
+
+export function moveElementToEnd(array: number[], toMove: number) {
+  let left = 0
+  let right = array.length - 1
+  while (left < right) {
+    while (left < right && array[right] === toMove) {
+      right--
+    }
+    const num = array[left]
+    if (num === toMove) {
+      array[left] = array[right]
+      array[right] = num
+    }
+    left++
+  }
+  return array
+}
