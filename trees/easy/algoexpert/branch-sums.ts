1a78605cd4ea83807b36b6e1859fc21deb18349e
Philip Phlek <pphlek@gmail.com>
2024-01-07 09:51:18 -0500

    solved algoexpert branch sums

---
+++ trees/easy/algoexpert/branch-sums.ts
@@ -0,0 +1,66 @@
+/*
+https://www.algoexpert.io/questions/branch-sums
+
+Write a function that takes in a Binary Tree and returns a list of its 
+branch sums ordered from leftmost branch sum to rightmost branch sum.
+
+A branch sum is the sum of all values in a Binary Tree branch. A Binary Tree
+branch is a path of nodes in a tree that starts at the root node and ends at
+any leaf node.
+
+Each BinaryTree node has an integer value, a left child node, and a right
+child node. Children nodes can either be BinaryTree nodes themselves or
+None / null.
+
+Sample Input
+tree =     1
+        /     \
+      2       3
+    /   \    /   \
+  4     5  6     7
+/   \  /
+8    9 10
+
+Sample Output
+[15, 16, 18, 10, 11]
+// 15 == 1 + 2 + 4 + 8
+// 16 == 1 + 2 + 4 + 9
+// 18 == 1 + 2 + 5 + 10
+// 10 == 1 + 3 + 6
+// 11 == 1 + 3 + 7
+*/
+
+class BinaryTree {
+  value: number
+  left: BinaryTree | null
+  right: BinaryTree | null
+
+  constructor(value: number) {
+    this.value = value
+    this.left = null
+    this.right = null
+  }
+}
+
+export function branchSums(root: BinaryTree): number[] {
+  const sums: number[] = []
+  dfsHelper(root, 0, sums)
+  return sums
+}
+
+function dfsHelper(
+  node: BinaryTree | null,
+  runningSum: number,
+  sums: number[]
+) {
+  if (!node) {
+    return
+  }
+  const newSum = runningSum + node.value
+  if (!node.left && !node.right) {
+    sums.push(newSum)
+    return
+  }
+  dfsHelper(node.left, newSum, sums)
+  dfsHelper(node.right, newSum, sums)
+}
